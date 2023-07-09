f19c77398b3cf843eb841b1247bbd9d74beca854
Santiago <santiagovalencialeon@gmail.com>
2023-07-09 17:30:53 -0500

    backend structure

---
+++ backend/app/routes/index.routes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const router = express.Router();
+
+// Ruta principal (base path /api)
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the API' });
+});
+
+// Rutas para los usuarios (base path: /api/users)
+// router.use('/users', userRoutes)
+
+// Rutas para los productos (base path: /api/products)
+// router.use('/products', productRoutes)
+
+// Rutas para los pedidos (base path: /api/orders)
+// router.use('/orders', orderRoutes)
+
+export default router;
