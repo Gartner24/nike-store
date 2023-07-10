f831279fe064b583c3db2dfd49207d7f2d477f6b
Santiago <santiagovalencialeon@gmail.com>
2023-07-09 23:29:04 -0500

    errores de juan

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,57 +5,57 @@ const Header = () => {
         <header>
 			<h1>Productos</h1>
 
-			<div class="container-icon">
-				<div class="container-cart-icon">
+			<div className="container-icon">
+				<div className="container-cart-icon">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
-						stroke-width="1.5"
+						strokeWidth="1.5"
 						stroke="currentColor"
-						class="icon-cart"
+						className="icon-cart"
 					>
 						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 							d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
 						/>
 					</svg>
-					<div class="count-products">
+					<div className="count-products">
 						<span id="contador-productos">0</span>
 					</div>
 				</div>
 
-				<div class="container-cart-products hidden-cart">
-					<div class="row-product hidden">
-						<div class="cart-product">
-							<div class="info-cart-product">
-								<span class="cantidad-producto-carrito">1</span>
-								<p class="titulo-producto-carrito">Zapatos Nike</p>
-								<span class="precio-producto-carrito">$80</span>
+				<div className="container-cart-products hidden-cart">
+					<div className="row-product hidden">
+						<div className="cart-product">
+							<div className="info-cart-product">
+								<span className="cantidad-producto-carrito">1</span>
+								<p className="titulo-producto-carrito">Zapatos Nike</p>
+								<span className="precio-producto-carrito">$80</span>
 							</div>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
-								stroke-width="1.5"
+								strokeWidth="1.5"
 								stroke="currentColor"
-								class="icon-close"
+								className="icon-close"
 							>
 								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
+									strokeLinecap="round"
+									strokeLinejoin="round"
 									d="M6 18L18 6M6 6l12 12"
 								/>
 							</svg>
 						</div>
 					</div>
 
-					<div class="cart-total hidden">
+					<div className="cart-total hidden">
 						<h3>Total:</h3>
-						<span class="total-pagar">$200</span>
+						<span className="total-pagar">$200</span>
 					</div>
-					<p class="cart-empty">El carrito está vacío</p>
+					<p className="cart-empty">El carrito está vacío</p>
 				</div>
 			</div>
 		</header>
--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,23 +1,40 @@
-import React from "react";
-import { data } from "../data";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import { data } from "../data";
 
 const ProductList = () => {
-  return (
-    <div className="container-items">
-      {data.map(product => (
-        <div className="item" key={product.id}>
-          <figure>
-            <img src={product.img} alt={product.nameProduct} />
-          </figure>
-          <div className="info-product">
-            <h2>{product.nameProduct}</h2>
-            <p className='price'>${product.price}</p>
-            <button>Añadir al carrito</button>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+	const [products, setProducts] = useState([]);
+
+  
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:8080/api/products')
+			.then((res) => {
+				setProducts(res.data);
+        console.log(res.data)
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	return (
+		<div className='container-items'>
+			{/* {data.map((product) => (
+				<div className='item' key={product.id}>
+					<figure>
+						<img src={product.img} alt={product.nameProduct} />
+					</figure>
+					<div className='info-product'>
+						<h2>{product.nameProduct}</h2>
+						<p className='price'>${product.price}</p>
+						<button>Añadir al carrito</button>
+					</div>
+				</div>
+			))} */}
+		</div>
+	);
 };
 
 export default ProductList;
