741f43fba15a5d0701b85f744c829eef86be4322
Juan Pablo <juan.sanchez8@utp.edu.co>
2023-07-11 23:46:26 -0500

    Cart Window and Cart icon, both responsive

--- frontend/src/components/CartWindow.jsx
+++ frontend/src/components/CartWindow.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import './css/CartWindow.css';
 
 const CartWindow = ({ onClose }) => {
+    const [cartItems, setCartItems] = useState([]);
+
+    const addToCart = (product) => {
+        setCartItems([...cartItems, product]);
+    };
+
     return (
         <div className="cart-window">
             <h2>Carrito de compras</h2>
             <ul className="cart-list">
-                <li>Producto 1</li>
-                <li>Producto 2</li>
-                <li>Producto 3</li>
+                {cartItems.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
             </ul>
-            <button className = "close-button" onClick={onClose}>Cerrar</button>
+            <button className="close-button" onClick={onClose}>
+                Cerrar
+            </button>
         </div>
     );
 };
 
 export default CartWindow;
+
--- frontend/src/components/ShoppingCart.jsx
+++ frontend/src/components/ShoppingCart.jsx
@@ -1,2 +1,35 @@
 import React from "react";
+import CartWindow from "./CartWindow";
+
+const ProductPage = () => {
+    const handleCloseCart = () => {
+        // Lógica para cerrar la ventana del carrito
+    };
+
+    const handleAddToCart = (product) => {
+        // Lógica para agregar el producto al carrito
+        console.log("Agregado al carrito:", product);
+    };
+
+    return (
+        <div>
+            {/* Aquí va el contenido de la página */}
+            <button onClick={() => handleAddToCart("Producto 1")}>
+                Agregar Producto 1 al carrito
+            </button>
+            <button onClick={() => handleAddToCart("Producto 2")}>
+                Agregar Producto 2 al carrito
+            </button>
+            <button onClick={() => handleAddToCart("Producto 3")}>
+                Agregar Producto 3 al carrito
+            </button>
+
+            {/* Ventana emergente del carrito */}
+            <CartWindow onClose={handleCloseCart} />
+        </div>
+    );
+};
+
+export default ProductPage;
+
 
