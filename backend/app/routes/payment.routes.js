22144aea5ff3bf06cd0d711ecfc96e98ad2861ee
Santiago <santiagovalencialeon@gmail.com>
2023-07-11 23:58:20 -0500

    feat payment

--- backend/app/routes/payment.routes.js
+++ backend/app/routes/payment.routes.js
@@ -8,11 +8,32 @@ const paymentRouter = express.Router();
 // Ruta para procesar el pago (http://localhost:8080/api/success)
 paymentRouter.get('/success', async (req, res) => {
 	try {
-		const { totalPrice, userID } = req.query; // Utilizamos req.query en lugar de req.body para obtener los parámetros enviados desde la pasarela de pago
-		console.log('userID', userID);
-		console.log('PRUEBA');
+		const { totalPrice, userID, shippingAddress } = req.query; // Utilizamos req.query en lugar de req.body para obtener los parámetros enviados desde la pasarela de pago
+		
+		// Crear la orden en la base de datos
+		const order = await Order.create({
+			userID,
+			totalPrice,
+			shippingAddress,
+		});
 
-		// 3.1 Si el pago es exitoso, cambiar el estado de los carritos a "Ordered" y crear la orden
+		// 3.1.1 Actualizar el inventario restando la cantidad vendida
+		const activeCarts = await ShoppingCart.findAll({
+			where: {
+			  userID,
+			  cartStatus: 'Active'
+			}
+		  });
+
+		for (const cart of activeCarts) {
+			const inventory = await Inventory.findOne({
+				where: { productID: cart.productID },
+			});
+
+			await inventory.decrement('quantity', { by: cart.quantity });
+		}
+		
+		// 3 Si el pago es exitoso, cambiar el estado de los carritos a "Ordered" y crear la orden
 		await ShoppingCart.update(
 			{ cartStatus: 'Ordered' },
 			{
@@ -22,24 +43,10 @@ paymentRouter.get('/success', async (req, res) => {
 				},
 			}
 		);
-		const shippingAddress = 'Calle 123';
-		// Crear la orden en la base de datos
-		const order = await Order.create({
-			userID,
-			totalPrice,
-			shippingAddress,
-		});
-		// 3.1.1 Actualizar el inventario restando la cantidad vendida
-		for (const cart of activeCarts) {
-			const inventory = await Inventory.findOne({
-				where: { productID: cart.productID },
-			});
 
-			console.log('PRUEBA 4');
-			await inventory.decrement('quantity', { by: cart.quantity });
-		}
-
-		return res.status(200).json({ message: 'Pago exitoso', order });
+		// return res.status(200).json({ message: 'Pago exitoso', order });
+		// CLose the window
+		return res.status(200).send('<script>window.close()</script>');
 	} catch (error) {
 		console.log(error);
 		return res.status(500).json({ message: 'Error al procesar el pago' });
@@ -52,42 +59,3 @@ paymentRouter.get('/cancel', (req, res) => {
 });
 
 export default paymentRouter;
-
-// // Ruta para procesar el pago
-// paymentRouter.get('/success', async (req, res) => {
-// 	// 3.1 Si el pago es exitoso, cambiar el estado de los carritos a "Ordered" y crear la orden
-// 	await ShoppingCart.update(
-// 		{ cartStatus: 'Ordered' },
-// 		{
-// 			where: {
-// 				userID,
-// 				cartStatus: 'Active',
-// 			},
-// 		}
-// 	);
-// 	// Crear la orden en la base de datos
-// 	const order = await Order.create({
-// 		userID,
-// 		totalPrice,
-// 		shippingAddress,
-// 		orderStatus: 'Ordered',
-// 	});
-
-// 	// 3.1.1 Actualizar el inventario restando la cantidad vendida
-// 	for (const cart of activeCarts) {
-// 		const inventory = await Inventory.findOne({
-// 			where: { productID: cart.productID },
-// 		});
-
-// 		await inventory.decrement('quantity', { by: cart.quantity });
-// 	}
-
-// 	res.status(200).json({ message: 'Pago exitoso', order });
-// });
-
-// // Ruta para cancelar el pago
-// paymentRouter.get('/cancel', (req, res) => {
-// 	res.status(200).json({ message: 'Pago cancelado' });
-// });
-
-// export default paymentRouter;
