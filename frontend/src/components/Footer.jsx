1154a62689b7ba7af3e3e03f507079e6f934190d
Juandagarc <juandagarce@gmail.com>
2023-07-13 16:06:52 -0500

    New footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,43 +1,37 @@
 import React from "react";
-import './css/Footer.css'
+import './css/Footer.css';
+
+const developers = [
+  { name: "Santiago Valencia Leon", github: "https://github.com/Gartner24" },
+  { name: "Adrián Fernando Gaitán Londoño", github: "https://github.com/adriancho91s" },
+  { name: "Arnovi Antonio Jimenez Velasquez", github: "https://github.com/Arnovii" },
+  { name: "Juan David Garcia Arce", github: "https://github.com/Juandagarc" },
+  { name: "Santiago Guevara Mendez", github: "https://github.com/DjSantech" },
+  { name: "Jeronimo Riveros Perea", github: "https://github.com/Max1mus5" },
+  { name: "Juan Pablo Sanchez Zapata", github: "https://github.com/jpsz2004" },
+  { name: "Julian Andres Corral Gomez", github: "https://github.com/JulianCorralG" },
+];
+
 const Footer = () => {
   return (
     <footer>
       <div className="footer-container">
-        <div className="footer-section">
-          <h4>GIFT CARDS</h4>
+        <div className="footer-developers">
+          <p>Developers:</p>
+          <ul>
+            {developers.map((developer, index) => (
+              <li key={index}>
+                <a href={developer.github} target="_blank" rel="noopener noreferrer">
+                  {developer.name}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="footer-section">
-          <h4>FIND A STORE</h4>
-        </div>
-        <div className="footer-section">
-          <h4>NIKE JOURNAL</h4>
-        </div>
-        <div className="footer-section">
-          <h4>BECOME MEMBER</h4>
-        </div>
-        <div className="footer-section">
-          <h4>STUDENT DISCOUNT</h4>
-        </div>
-        <div className="footer-section">
-          <h4>FEEDBACK</h4>
-        </div>
-        <div className="footer-section">
-          <h4>PROMOTIONAL CODES</h4>
-        </div>
-        <div className="footer-section">
-          <h4>HELP</h4>
+        <div className="footer-legal">
+          <p>© 2023 Nike, Inc. Todos los derechos reservados</p>
         </div>
       </div>
-        <div className="footer-legal">
-          <p>Guides     |    </p>
-          <p>Terms of Use    |    </p>
-          <p>Terms of Sale    |     </p>
-          <p>Legal Notice    |    </p>
-          <p>Privacy & Cookies Policy    |    </p>
-          <p>Cookie settings    |    </p>
-          <p>© 2023 Nike, Inc. All rights reserved </p>
-        </div>
     </footer>
   );
 };
